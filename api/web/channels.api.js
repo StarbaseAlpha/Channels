a36e2665bb5146407043973edfee6342b4394683
Mike Fuller <emailthemike@gmail.com>
2018-11-22 01:43:42 -0500

    added project files

---
+++ api/web/channels.api.js
@@ -0,0 +1,131 @@
+'use strict';
+
+function ChannelsAPI(serverPath) {
+
+  const ParsePath = (requestPath) => {
+
+    let reqPath;
+
+    if (requestPath) {
+      reqPath = requestPath.toString();
+    } else {
+      reqPath = '/';
+    }
+
+    let parts = reqPath.toString()
+      .replace(/\/\//g, '/')
+      .replace(/^\//, '')
+      .replace(/\/$/, '')
+      .replace(/\/\//g, '/')
+      .split('/');
+
+    let channel = '/' + parts.slice(0, -1).join('/');
+    let key = parts.slice(-1)[0];
+    let slash = "";
+    if (channel !== '/') {
+      slash = '/';
+    }
+    let path = channel + slash + key;
+
+    return {
+      "path": path,
+      "channel": channel,
+      "key": key,
+      "slash": slash
+    };
+  };
+
+
+  let token = null;
+
+  let channels = {};
+
+  const request = (req) => {
+    return fetch(serverPath,{
+      "method":"POST",
+      "headers":{
+        "content-type":"application/json"
+      },
+      "body":JSON.stringify({
+        "token": token || req.token || "",
+        "method":req.method || "",
+        "path":req.path || "",
+        "data":req.data || {}
+      })
+    }).then(response=>{
+      return response.json();
+    });
+  };
+
+  const setToken = (newToken) => {
+    token = newToken;
+    return true;
+  };
+
+  const Put = (path,data) => {
+    return request({"method":"put","path":path,"data":data});
+  };
+
+  const Get = (path) => {
+    return request({"method":"get","path":path});
+  };
+
+  const Del = (path) => {
+    return request({"method":"del","path":path});
+  };
+
+  const List = (path,data) => {
+    return request({"method":"list","path":path,"data":data});
+  };
+
+  const Channel = (requestPath) => {
+
+    let parsed = ParsePath(requestPath);
+    let parsedPath = parsed.path;
+
+    let chan = {
+
+      "channel": () => {
+        return parsedPath;
+      },
+
+      "setToken": (token) => {
+        setToken(token);
+      },
+
+      "put": (data) => {
+        return Put(parsedPath, data);
+      },
+
+      "get": () => {
+        return Get(parsedPath);
+      },
+
+      "del": () => {
+        return Del(parsedPath);
+      },
+
+      "list": (query) => {
+        return List(parsedPath, query);
+      },
+
+      "path": (path) => {
+        if (!path) {
+          path = "/";
+        }
+        let slash = "";
+        if (parsedPath !== '/') {
+          slash = "/";
+        }
+        return Channel(parsedPath + slash + path);
+      },
+
+    };
+
+    return chan;
+
+  };
+
+  return Channel('/');
+
+}
---
+++ channels.js
@@ -0,0 +1,287 @@
+'use strict';
+
+// Channels - The db parameter should be a database or memstore object
+function Channels(db) {
+
+  let onEvent;
+
+  const eventHandler = (e) => {
+    if (onEvent && typeof onEvent === 'function') {
+      onEvent(e);
+    }
+  };
+
+
+  // ParsePath - Parses the requested Path into the useful parts
+  const ParsePath = (requestPath) => {
+
+    let reqPath;
+
+    if (requestPath) {
+      reqPath = requestPath.toString();
+    } else {
+      reqPath = '/';
+    }
+
+    // Removing trailing, leading, and double slashes and split on slash
+    let parts = reqPath.toString()
+      .replace(/\/\//g, '/')
+      .replace(/^\//, '')
+      .replace(/\/$/, '')
+      .replace(/\/\//g, '/')
+      .split('/');
+
+    // Useful parts
+    let channel = '/' + parts.slice(0, -1).join('/');
+    let key = parts.slice(-1)[0];
+    let slash = "";
+    if (channel !== '/') {
+      slash = '/';
+    }
+    let path = channel + slash + key;
+
+    return {
+      "path": path,
+      "channel": channel,
+      "key": key,
+      "slash": slash
+    };
+  };
+
+  // Channel - return an object with CRUD operations directed to the request path
+  const Channel = (db, requestPath) => {
+
+    let parsed = ParsePath(requestPath);
+    let parsedPath = parsed.path;
+
+    let chan = {
+
+      "channel": () => {
+        return parsedPath;
+      },
+
+      "put": (data) => {
+        return Put(db, parsedPath, data);
+      },
+
+      "get": () => {
+        return Get(db, parsedPath);
+      },
+
+      "del": () => {
+        return Del(db, parsedPath);
+      },
+
+      "list": (query) => {
+        return List(db, parsedPath, query);
+      },
+
+      // the path method provides an instance of Channel pointing to a child path of its parent
+      "path": (path) => {
+        if (!path) {
+          path = "/";
+        }
+        let slash = "";
+        if (parsedPath !== '/') {
+          slash = "/";
+        }
+        return Channel(db, parsedPath + slash + path);
+      },
+
+    };
+
+    return chan;
+
+  };
+
+  const Put = (db, requestPath, data) => {
+    return new Promise((resolve, reject) => {
+
+      if (!data) {
+        return reject({
+          "code": 400,
+          "message": "Data is required."
+        });
+      }
+
+      if (requestPath.match(/\!/)) {
+        return reject({
+          "code": 400,
+          "message":"Invalid Path. The exclamation point (!) is a reserved character."
+        });
+      }
+
+      let {
+        path,
+        channel,
+        key,
+        slash
+      } = ParsePath(requestPath);
+
+      db.put('!' + channel + '!' + key, data).then(result => {
+
+        let response = {
+          "event": result.event,
+          "path": channel + slash + (key || ""),
+          "channel": channel,
+          "key": key || "",
+          "timestamp": result.timestamp
+        };
+
+        if (channel === '/' && (!key || key === '')) {
+          eventHandler(response);
+          return resolve(response);
+        }
+
+        Get(db, channel).then(found => {
+          eventHandler(response);
+          resolve(response);
+        }).catch(err => {
+          Put(db, channel, {}).then(written => {
+            eventHandler(response);
+            resolve(response);
+          }).catch(reject);
+
+        });
+
+      }).catch(reject);
+
+    });
+  };
+
+  const Get = (db, requestPath) => {
+    return new Promise((resolve, reject) => {
+
+      let {
+        path,
+        channel,
+        key,
+        slash
+      } = ParsePath(requestPath);
+
+      db.get('!' + channel + '!' + key).then(result => {
+
+        if (!result.value) {
+          return reject({
+            "code": 404,
+            "message": "Not Found"
+          });
+        }
+
+        resolve({
+          "path": channel + slash + key,
+          "channel": channel,
+          "key": key,
+          "data": result.value
+        });
+
+      }).catch(reject);
+
+    });
+  };
+
+  const Del = (db, requestPath) => {
+    return new Promise((resolve, reject) => {
+
+      let {
+        path,
+        channel,
+        key,
+        slash
+      } = ParsePath(requestPath);
+
+      db.list({
+
+        "gt": "!" + channel + slash + key + "!",
+        "lt": "!" + channel + slash + key + '\uffff' + "/"
+
+      }).then(results => {
+
+        results.unshift('!' + channel + '!' + key);
+
+        db.del(results).then(deleted => {
+          let paths = deleted.keys.map(val => {
+            return val.replace(/^\!/, '').replace(/\!/g, '/').replace(/\/\//g, '/').replace(/\/\//g,'/');
+          });
+          let e = {
+            "event": deleted.event,
+            "paths":paths,
+            "timestamp": deleted.timestamp
+          };
+          eventHandler(e);
+          resolve(e);
+        });
+
+      });
+
+    });
+  };
+
+  const List = (db, requestPath = "/", query = {}) => {
+    return new Promise((resolve, reject) => {
+      if (typeof query !== 'object') {
+        query = {};
+      }
+      let channel = requestPath.toString();
+      let slash = "";
+      if (channel !== "/") {
+        slash = "/";
+        channel = '/' + channel.replace(/^\//, '').replace(/\/$/, '');
+      }
+      let gt = "!" + channel + ("!" + (query.gt || ""));
+      let lt = "!" + channel + ("!" + (query.lt || '\uffff'));
+      let deep = query.deep || false;
+      if (deep) {
+        lt = "!" + channel + slash + '\uffff';
+      }
+      db.list({
+        "gt": gt,
+        "lt": lt,
+        "reverse": query.reverse || false,
+        "values": query.values || false,
+        "limit": query.limit || null
+      }).then(results => {
+        let data = results.map(val => {
+          if (typeof val !== 'object') {
+            val = {
+              "key": val
+            };
+          }
+          let channel = val.key.replace(/^\!/, '').split('!')[0];
+          let key = val.key.replace(/^\!/, '').split('!')[1] || '/';
+          let slash = "";
+          if (channel !== '/') {
+            slash = "/";
+          }
+          let path = channel + slash + key;
+          let result = {
+            "path": path,
+            "channel": channel,
+            "key": key,
+          };
+          if (val.value) {
+            result.data = val.value;
+          }
+          return result;
+        });
+        resolve({
+          "data": data
+        });
+      });
+    });
+  };
+
+  let channel = Channel(db,'/');
+  channel.onEvent = (cb) => {
+    onEvent = cb;
+  };
+
+  return channel;
+
+}
+
+// Support for node.js
+if (typeof module !== 'undefined' && module && module.exports) {
+  module.exports = Channels;
+}
+
